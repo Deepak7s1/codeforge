3671faa67886e8f8348d24670159194fa36bcbd0
Adrian Yau <adrian.yau@oracle.com>
2015-10-23 18:26:27 -0700

    Learning Node and ES6 - sample scripts.

---
+++ nodejs/tutorials/http/hello-world.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const serverPort = 8124;
+const serverHostname = "127.0.0.1";
+
+http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Word!\n');
+}).listen(serverPort, serverHostname);
+
+// Using ES6 string interpolation.
+console.log(`Server running at http://${serverHostname}:${serverPort}/`);
